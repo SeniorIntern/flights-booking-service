febef08803f6c13b02b8c3e03b2529e134cd2c43
SeniorIntern <tnikhil.official@gmail.com>
2024-03-18 20:32:16 +0545

    Add POST booking api

--- src/services/booking-service.js
+++ src/services/booking-service.js
@@ -1,1 +1,29 @@
-module.exports = {};
+const axios = require('axios');
+
+const { StatusCodes } = require('http-status-codes');
+const { BookingRepository } = require('../repositories');
+const { ServerConfig } = require('../config');
+const db = require('../models');
+const AppError = require('../utils/errors/app-error');
+
+async function createBooking(data) {
+  return new Promise((resolve, reject) => {
+    const result = db.sequelize.transaction(async function bookingImpl() {
+      const flight = await axios.get(
+        `${ServerConfig.FLIGHT_SERVICE}/api/v1/flights/${data.flightId}`
+      );
+      const flightData = flight.data.data;
+      console.log('data=', data);
+      if (data.noOfSeats > flightData.totalSeats) {
+        reject(
+          new AppError('Not enough seats available', StatusCodes.BAD_REQUEST)
+        );
+      }
+      resolve(true);
+    });
+  });
+}
+
+module.exports = {
+  createBooking
+};
